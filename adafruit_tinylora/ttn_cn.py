e2f8ea17d7fc86e28051c5c927614973fcd869c0
IAMLIUBO <imliubo@makingfun.xyz>
2021-01-15 22:38:00 +0800

    Modified ttn_cn.py License.

--- adafruit_tinylora/ttn_cn.py
+++ adafruit_tinylora/ttn_cn.py
@@ -1,6 +1,7 @@
 # The MIT License (MIT)
 #
 # Copyright (c) 2018 Brent Rubell for Adafruit
+# Modified by IAMLIUBO, 2021
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
